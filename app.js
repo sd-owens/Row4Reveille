6779a1c545f707a9f1655028bdb7e641dde246e8
Steve Owens <sd.owens@outlook.com>
2019-09-20 15:04:59 -0700

    initialized home and landing pages

---
+++ app.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const express = require("express"),
+	app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", async (req, res) => {
+	await res.render("landing");
+});
+
+app.get("/home", async (req, res) => {
+	await res.render("home");
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP, async () => {
+	await console.log("Server listening on port 3000");
+});
