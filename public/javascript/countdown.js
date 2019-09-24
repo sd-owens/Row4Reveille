4eb6eded9002dddbc2bb3b5f96224da04c3301e1
Steve Owens <sd.owens@outlook.com>
2019-09-24 08:40:48 -0700

    added facebook widget feature

--- public/javascript/countdown.js
+++ public/javascript/countdown.js
@@ -1,4 +1,4 @@
-var deadline = new Date("Jan 18, 2020 08:00:00").getTime();
+var deadline = new Date("Jan 18, 2020 09:00:00").getTime();
 var x = setInterval(function() {
 	var now = new Date().getTime();
 	var t = deadline - now;
