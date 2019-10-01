e0666eb80824e016234af21cf46b31e6a1353d36
Steve Owens <sd.owens@outlook.com>
2019-10-01 07:14:24 -0700

    modified prettier and eslint settings

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express"),
-	app = express();
+const express = require('express');
 
-app.set("view engine", "ejs");
+const app = express();
 
-app.use(express.static(__dirname + "/public"));
+app.set('view engine', 'ejs');
 
-app.get("/", async (req, res) => {
-	await res.render("landing");
+app.use(express.static(`${__dirname}/public`));
+
+app.get('/', async (req, res) => {
+	await res.render('landing');
 });
 
-app.get("/home", async (req, res) => {
-	await res.render("home");
+app.get('/home', async (req, res) => {
+	await res.render('home');
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP, async () => {
-	await console.log("Server listening on port 3000");
+	await console.log('Server listening on port 3000');
 });
