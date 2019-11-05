fd4455d1a2459bf669618e8b63a22c032cd2035d
Steve Owens <sd.owens@outlook.com>
2019-11-04 18:57:45 -0800

    fixed vh of R4R image

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
 
 function requireHTTPS(req, res, next) {
-	// The 'x-forwarded-proto' check is for Heroku
-	if (
-		!req.secure &&
+  // The 'x-forwarded-proto' check is for Heroku
+  if (
+    !req.secure &&
     req.get('x-forwarded-proto') !== 'https' &&
     process.env.NODE_ENV !== 'development'
-	) {
-		res.redirect(`https://${req.get('host')}${req.url}`);
-	}
-	next();
+  ) {
+    res.redirect(`https://${req.get('host')}${req.url}`);
+  }
+  next();
 }
 
 const express = require('express');
@@ -23,13 +23,13 @@ app.use(requireHTTPS);
 app.use(express.static(`${__dirname}/public`));
 
 app.get('/', async (req, res) => {
-	await res.render('home');
+  await res.render('home');
 });
 
 app.get('/home', async (req, res) => {
-	await res.render('home');
+  await res.render('home');
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP, async () => {
-	await console.log('Server listening on port 3000');
+  await console.log('Server listening on port 3000');
 });
